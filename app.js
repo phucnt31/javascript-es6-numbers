40d3c0e6d3cba145d87fea9ef45b032d145a1ffa
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-26 11:28:24 +0700

    Select elements

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('numbers project');
+const items = [...document.querySelectorAll(".number")];
+
+const updateCount = (el) => {
+  console.log(el);
+};
+
+items.forEach((item) => {
+  updateCount(item);
+});
