8b66903c095aeb9568f68da753492448bcba18b1
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-26 11:31:11 +0700

    Initial values

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const items = [...document.querySelectorAll(".number")];
 
 const updateCount = (el) => {
-  console.log(el);
+  const value = parseInt(el.dataset.value);
+  const increment = Math.ceil(value / 1000);
+  let initialValue = 0;
 };
 
 items.forEach((item) => {
